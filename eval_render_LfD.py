f91db2396756fb144f842c2d4f6f4c244e80eed4
zizhaowang <zzwang@umich.edu>
2021-03-02 03:23:28 -0600

    add timeout for eval and rend

--- eval_render_LfD.py
+++ eval_render_LfD.py
@@ -3,6 +3,13 @@ import subprocess
 cmds = ["/home/users/zizhaowang/hallucination/LfH/eval.py",
         "/home/users/zizhaowang/hallucination/LfD_3D/render_demo_3D.py",
         "/home/users/zizhaowang/hallucination/LfD_3D/LfD_main.py"]
-for cmd in cmds:
-    p = subprocess.Popen(["python", cmd])
-    p.communicate()
+for i, cmd in enumerate(cmds):
+    p = subprocess.Popen(["python", "-u", cmd])
+    if i < 2:
+        try:
+            outs, errs = p.communicate(timeout=1800)
+        except subprocess.TimeoutExpired:
+            p.kill()
+            outs, errs = p.communicate()
+    else:
+        p.communicate()
