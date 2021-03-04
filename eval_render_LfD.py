bd3c9748a3f9d94be54aa72a9dc8c1a7f6299272
zizhaowang <zzwang@umich.edu>
2021-03-04 03:53:25 -0600

    no need to time out render obs

--- eval_render_LfD.py
+++ eval_render_LfD.py
@@ -5,7 +5,7 @@ cmds = ["/home/users/zizhaowang/hallucination/LfH/eval.py",
         "/home/users/zizhaowang/hallucination/LfD_3D/LfD_main.py"]
 for i, cmd in enumerate(cmds):
     p = subprocess.Popen(["python", "-u", cmd])
-    if i < 2:
+    if i < 1:
         try:
             outs, errs = p.communicate(timeout=1800)
         except subprocess.TimeoutExpired:
